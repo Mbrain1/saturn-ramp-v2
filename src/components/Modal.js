77b0f2ecff2eb6554dfd7e14e0a6c9585f760ab1
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-04-22 10:18:56 +0100

    Update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,11 +5,12 @@ const Modal = ({ children, isOpen, setIsOpen, bodyClass}) => {
 
 	return (
 		 <section  className={`modal-wrapper ${isOpen ? 'show-modal' : 'hide-modal'} `}>
-           <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-0 -top-[20%] opacity-20" />
-            <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-[201] -left-[60%] -bottom-[20%] opacity-20" />
-          <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-[201] -right-[40%] -bottom-[20%] opacity-20" />
+	      <div className="modal-inner-wrapper  justify-start  space-y-5 md:space-y-10">
 
-	      <div className="modal-inner-wrapper justify-start  space-y-5 md:space-y-10">
+             <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-[201] -top-[20%] opacity-20" />
+            <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-[201] -left-[60%] -bottom-[20%] opacity-20" />
+            <img src={`/assets/svgs/circular-bg.svg`} className="absolute z-[201] -right-[40%] -bottom-[20%] opacity-20" />
+
             <div className="relative z-[301] w-full md:px-10">
              <Navbar isContainer={false} />
              </div>
